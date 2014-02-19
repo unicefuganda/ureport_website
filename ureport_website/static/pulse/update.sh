93e40af9e502da0c14abfec936099211df973633
Martin Atukunda <matlads@gmail.com>
2014-02-19 09:31:00 +0300

    Add script to periodically update the pulse data

---
+++ ureport_website/static/pulse/update.sh
@@ -0,0 +1,4 @@
+curl http://ureport.ug/pulse/day/ > day.json
+curl http://ureport.ug/pulse/week/ > week.json
+curl http://ureport.ug/pulse/month/ > month.json
+curl http://ureport.ug/pulse/year/ > year.json
